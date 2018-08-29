9dd0229a952a8efd6f84a285683714b743e8096a
bialek <bialekrafal94@gmail.com>
2018-08-29 20:52:11 +0200

    add skills sections

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,10 @@
 var myFullpage = new fullpage('#fullpage', {
-    anchors: ['home', 'about', 'projects', 'psd', 'games', 'orders', 'contact'],
-    sectionsColor: ['', '#6CADAE', '#344043', '#498B91', '#85C7CC', '#ADD6D8', '#C6D8D9'],
+    anchors: ['home', 'about', 'skills', 'projects', 'psd', 'games', 'orders', 'contact'],
+    sectionsColor: ['','#8dbfc0', '#6CADAE', '#344043', '#498B91', '#85C7CC', '#ADD6D8', '#C6D8D9'],
     navigation: true,
     navigationPosition: 'right',
-    navigationTooltips: ['Home', 'About me', 'Projects', 'Psd to Html', 'Games', 'Paid orders', 'Contact'],
+    navigationTooltips: ['Home', 'About me', 'Skills', 'Projects', 'Psd to Html', 'Games', 'Paid orders', 'Contact'],
     responsiveWidth: 900,
     afterResponsive: function(isResponsive){
-
     }
-
 });
